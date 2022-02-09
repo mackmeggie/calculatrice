79fdc968c4a6f3e31be9172645c553248e2c5d64
mackmeggie <mack.meggie13@gmail.com>
2022-02-08 20:59:25 -0700

    index, style and script create and outline

---
+++ script.js
@@ -0,0 +1,31 @@
+const a = 5
+const b = 4 
+
+//Addition
+const add = function(a, b) {
+    let c = a + b;
+      if (a == 0 && b == 0) {
+      let c = 0
+    }
+    return c;
+  };
+
+//Subtraction
+const subtract = function(a, b) {
+    let c = a - b;
+    return c;
+  };
+
+//Multiplication
+const multiply = function(a, b) {
+    let c = a * b;
+    return c;
+}
+
+//Division
+const divide = function(a, b) {
+    let c = a / b;
+    return c;
+}
+
+console.log(divide(a, b))
